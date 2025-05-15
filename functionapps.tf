32cce1ee7eb4d86142ad9285d6222f3ae6c0b7ba
shaoniaogong <shaoniaogong@outlook.com>
2025-05-15 21:24:11 +1200

    updates

---
+++ functionapps.tf
@@ -0,0 +1,74 @@
+resource "azurerm_storage_account" "storage" {
+  name                     = "autdemostorage1234"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "autdemo-function-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "function" {
+  name                       = "autdemo-functionapp1234"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id            = azurerm_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    application_stack {
+      node_version = "18"
+    }
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "autdemo-law"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_monitor_diagnostic_setting" "storage_diagnostics" {
+  name                       = "diag-storage"
+  target_resource_id         = azurerm_storage_account.storage.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+
+  metric {
+    category = "Transaction"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "function_diagnostics" {
+  name                       = "diag-function"
+  target_resource_id         = azurerm_linux_function_app.function.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+
+  enabled_log {
+    category = "FunctionAppLogs"
+    retention_policy {
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+    retention_policy {
+      enabled = false
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type    = string
+  default = "Australia East"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "AUT-2025-demo_1"
+}
