e99ba56213384182cb486fe9ad24b3dbb3a01b79
shaoniaogong <shaoniaogong@outlook.com>
2025-05-19 17:38:51 +1200

    udpated functionapps

--- functionapps.tf
+++ functionapps.tf
@@ -29,50 +29,6 @@ resource "azurerm_linux_function_app" "function" {
   }
 }
 
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = var.log_analytics_workspace_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = var.log_analytics_workspace_sku
-  retention_in_days   = 30
-}
-
-resource "azurerm_monitor_diagnostic_setting" "storage_diagnostics" {
-  name                       = "diag-storage"
-  target_resource_id         = azurerm_storage_account.storage.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
-
-  metric {
-    category = "Transaction"
-    enabled  = true
-
-    retention_policy {
-      enabled = false
-    }
-  }
-}
-
-resource "azurerm_monitor_diagnostic_setting" "function_diagnostics" {
-  name                       = "diag-function"
-  target_resource_id         = azurerm_linux_function_app.function.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
-
-  enabled_log {
-    category = "FunctionAppLogs"
-    retention_policy {
-      enabled = false
-    }
-  }
-
-  metric {
-    category = "AllMetrics"
-    enabled  = true
-    retention_policy {
-      enabled = false
-    }
-  }
-}
-
 # Example of using the full GitHub source for a module (uncomment and adapt as needed):
 # module "function_app" {
 #   source = "github.com/aztfmod/terraform-azurerm-caf//modules/function_app?ref=5.7.14"
